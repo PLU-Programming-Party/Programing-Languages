cbb38daae1a474dea1bb279a500289f02dde09d8
rintrash <youa@plu.edu>
2023-06-19 14:31:51 -0700

    textarea box

--- my-app/src/Input.js
+++ my-app/src/Input.js
@@ -1,18 +1,18 @@
 import {useState} from 'react'
 export default function Input() {
-    const [text, setText] = useState('hello');
+    const [text, setText] = useState('');
 
     function handleChange(e) {
         setText(e.target.value);
     }
 
     return (
-        <>
-        <input value={text} onChange={handleChange} />
-        <p>You typed: {text}</p>
-        <button onClick={() => setText('hello')}>
-            Reset
-        </button>
-        </>
+        <div className="input">
+            <textarea value={text} onChange={handleChange} />
+            <p>You typed: {text}</p>
+            <button onClick={() => setText('')}>
+                Reset
+            </button>
+        </div>
     );
 }
