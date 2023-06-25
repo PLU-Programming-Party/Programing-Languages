f5808e1388c94c5033acfe018158df3837291f71
rintrash <youa@plu.edu>
2023-06-24 21:09:59 -0700

    Can input javascript

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,12 @@
 import './App.css';
-import Input from './Input'
-import Output from './Output'
+import Input from './Ide'
 
 function App() {
-  const srcDoc = //what the output will be displaying
-    `
-    <html>
-      <body>
-        <h1>testing</h1>
-      </body>
-    </html>
-    `;
 
   return (
     <div className="App">
         <div className="ide">
           <Input/>
-          <Output srcDoc={srcDoc}/>
         </div>
         
     </div>
---
+++ my-app/src/Ide.js
@@ -0,0 +1,36 @@
+import {useState} from 'react'
+export default function Input() {
+    const [text, setText] = useState('write js here');
+    
+    const srcDoc = 
+    `
+    <html>
+        <body>
+            <h1>Hello 5S</h1>
+            <script>${text}</script>
+        </body>
+        
+    </html>
+    `
+
+    function handleChange(e) {
+        setText(e.target.value);
+    }
+
+    return (
+        <div className="ide">
+            <textarea value={text} onChange={handleChange} />
+            <button onClick={() => setText('write js here')}>
+                Reset
+            </button>
+
+            <div className="output">
+                <iframe
+                sandbox="allow-scripts"
+                srcDoc={srcDoc}
+                title="output"
+                />
+            </div>
+        </div>
+    );
+}
