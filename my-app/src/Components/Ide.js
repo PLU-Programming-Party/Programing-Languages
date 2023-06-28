41a40dad7f9f94a1a7da2a9762880ed9bac4a652
rintrash <youa@plu.edu>
2023-06-28 16:04:57 -0700

    Run button(cant run duplicates)

--- my-app/src/Components/Ide.js
+++ my-app/src/Components/Ide.js
@@ -1,28 +1,30 @@
 import {useState} from 'react'
+
 export default function JsIde() {
     const [text, setText] = useState('write js here');
-    
-    const srcDoc = 
-    `
-    <html>
-        <body>
-            <h1>Hello 5S</h1>
-            <script>${text}</script>
-        </body>
-        
-    </html>
-    `
+    const [srcDoc, setSrcDoc] = useState('');
 
     function handleChange(e) {
         setText(e.target.value);
     }
-
     return (
         <div className="ide">
             <textarea value={text} onChange={handleChange} />
             <button onClick={() => setText('write js here')}>
                 Reset
             </button>
+            
+            <button onClick={() => setSrcDoc(
+                `
+                <html>
+                    <body>
+                        <h1>Hello 5S</h1>
+                        <script>${text}</script>
+                    </body>
+                    
+                </html>
+                            `
+            )}>Run</button>
 
             <div className="output">
                 <iframe
