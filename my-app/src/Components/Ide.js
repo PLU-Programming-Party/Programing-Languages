2af6f20ff54fbc7129410507ecb2c075b44ed5f4
rintrash <youa@plu.edu>
2023-06-28 16:56:41 -0700

    run button fixed

--- my-app/src/Components/Ide.js
+++ my-app/src/Components/Ide.js
@@ -2,10 +2,26 @@ import {useState} from 'react'
 
 export default function JsIde() {
     const [text, setText] = useState('write js here');
-    const [srcDoc, setSrcDoc] = useState('');
+    const [srcDoc, setSrcDoc] = useState(''); //srcDoc is the html code the user will manipulate
+    const [forceUpdate, setForceUpdate] = useState(0);
 
     function handleChange(e) {
         setText(e.target.value);
+    }
+
+    function rerenderSrcDoc() {
+        setSrcDoc(
+            `
+            <html>
+                <body>
+                    <h1>Hello 5S</h1>
+                    <script>${text}</script>
+                </body>
+                
+            </html>
+                        `
+        );
+        setForceUpdate(forceUpdate + 1); 
     }
     return (
         <div className="ide">
@@ -14,20 +30,11 @@ export default function JsIde() {
                 Reset
             </button>
             
-            <button onClick={() => setSrcDoc(
-                `
-                <html>
-                    <body>
-                        <h1>Hello 5S</h1>
-                        <script>${text}</script>
-                    </body>
-                    
-                </html>
-                            `
-            )}>Run</button>
+            <button onClick={rerenderSrcDoc}>Run</button>
 
             <div className="output">
                 <iframe
+                key={forceUpdate} //makes it so the iframe "changes" for a rerender 
                 sandbox="allow-scripts"
                 srcDoc={srcDoc}
                 title="output"
