da10ae3d0f599f5ab5b4ba0d6aa8f686703029f6
rintrash <youa@plu.edu>
2023-07-18 09:02:07 -0700

    Added chatgpt (text cuts off)

--- my-app/src/Pages/Home.js
+++ my-app/src/Pages/Home.js
@@ -1,10 +1,12 @@
 import JsIde from "../Components/Ide"
 import '../styles/Home.css'
+import Gpt from "../Components/Gpt"
 export default function Home() {
     return (
         <>
             <h3>In Home</h3>
             <JsIde></JsIde>
+            <Gpt></Gpt>
         </>
     )
 }
